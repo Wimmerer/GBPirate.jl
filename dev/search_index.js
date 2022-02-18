9f149b304d2552d65c362b0c3abe8b92c6d6e8f4
Documenter.jl <documenter@juliadocs.github.io>
2022-02-18 04:37:03 +0000

    build based on bd1deb5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GBPirate","category":"page"},{"location":"#GBPirate","page":"Home","title":"GBPirate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GBPirate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GBPirate]","category":"page"}]
+}
