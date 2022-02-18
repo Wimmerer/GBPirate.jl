1e0cf0fb81b55b53ec631f4390d3827d540fa5d3
Wimmerer <kimmerer@mit.edu>
2022-02-18 00:38:11 -0500

    initial method

--- src/GBPirate.jl
+++ src/GBPirate.jl
@@ -1,5 +1,26 @@
 module GBPirate
 
-# Write your package code here.
+import SparseArrays
+using SuiteSparseGraphBLAS
+import LinearAlgebra: mul!
 
+function mul!(C::StridedVecOrMat{T}, A::SparseArrays.SparseMatrixCSC{T, Int64}, B::StridedVecOrMat{T}, α::Number, β::Number) where {T}
+    Cgb = GBMatrix{T}(size(C))
+    gbset(Cgb, :format, :bycol)
+    gbset(Cgb, :sparsity_control, :full)
+    Agb = GBMatrix{T}(size(A))
+    SuiteSparseGraphBLAS._packcscmatrix!(Agb, A.colptr, A.rowval, A.nzval)
+    SuiteSparseGraphBLAS._makeshallow!(Agb)
+    
+    Bgb = GBMatrix{T}(size(B))
+    SuiteSparseGraphBLAS._packdensematrix!(Bgb, B)
+    SuiteSparseGraphBLAS._makeshallow!(Bgb)
+    mul!(Cgb, α .* Agb, Bgb; accum=+)
+    SuiteSparseGraphBLAS._unpackcscmatrix!(Agb)
+    SuiteSparseGraphBLAS._unpackdensematrix!(Bgb)
+    Ctemp = SuiteSparseGraphBLAS._unpackdensematrix!(Cgb)
+    copyto!(C, Ctemp)
+    ccall(:jl_free, Cvoid, (Ptr{T},), pointer(Ctemp))
+    return C
+end
 end
