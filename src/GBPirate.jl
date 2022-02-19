08053a31141ccab1d603cc0872233e445eac4d61
Wimmerer <kimmerer@mit.edu>
2022-02-18 23:20:37 -0500

    fix 0 vs 1 based indexing

--- src/GBPirate.jl
+++ src/GBPirate.jl
@@ -4,10 +4,9 @@ import SparseArrays
 using SuiteSparseGraphBLAS
 import LinearAlgebra: mul!
 
-function mul!(C::StridedVecOrMat{T}, A::SparseArrays.SparseMatrixCSC{T, Int64}, B::StridedVecOrMat{T}, α::Number, β::Number) where {T}
-    Cgb = GBMatrix{T}(size(C))
-    gbset(Cgb, :format, :bycol)
-    gbset(Cgb, :sparsity_control, :full)
+export gbmul!
+function gbmul!(C::StridedVecOrMat{T}, A::SparseArrays.SparseMatrixCSC{T, Int64}, B::StridedVecOrMat{T}, α::Number, β::Number) where {T}
+    
     Agb = GBMatrix{T}(size(A))
     SuiteSparseGraphBLAS._packcscmatrix!(Agb, A.colptr, A.rowval, A.nzval)
     SuiteSparseGraphBLAS._makeshallow!(Agb)
@@ -15,10 +14,25 @@ function mul!(C::StridedVecOrMat{T}, A::SparseArrays.SparseMatrixCSC{T, Int64}, 
     Bgb = GBMatrix{T}(size(B))
     SuiteSparseGraphBLAS._packdensematrix!(Bgb, B)
     SuiteSparseGraphBLAS._makeshallow!(Bgb)
+ 
+    Cgb = GBMatrix(size(C), convert(T, α))
+    gbset(Cgb, :format, :bycol)
+    gbset(Cgb, :sparsity_control, :full)
+    mul!(Cgb, Agb, Bgb; accum=*)
 
-    mul!(Cgb, α .* Agb, Bgb; accum=+)
+    # Done with Agb and Bgb now.
+    # They do not need to be unpacked since they're shallow.
+    # But packing modified the index vectors of A.
+    A.colptr .+= 1
+    A.rowval .+= 1
+    return Cgb
     Ctemp = SuiteSparseGraphBLAS._unpackdensematrix!(Cgb)
-    copyto!(C, Ctemp)
+
+    if iszero(β)
+        copyto!(C, Ctemp)
+    else
+        C .*= Ctemp
+    end
     ccall(:jl_free, Cvoid, (Ptr{T},), pointer(Ctemp))
     return C
 end
